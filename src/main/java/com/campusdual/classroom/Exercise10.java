2a8fa096ea98af2bc42e2f19b152e7b19ad6a968
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2024-11-08 20:00:42 +0100

    Resolver ejercicio10

--- src/main/java/com/campusdual/classroom/Exercise10.java
+++ src/main/java/com/campusdual/classroom/Exercise10.java
@@ -4,23 +4,46 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Exercise10 {
 
-    //TODO ↓
     // Imprimir el color de las pelotas que salgan, hasta que salgan 2 de color azul
     // El mensaje a imprimir es → La bola es de color: COLOR_BOLA
     // COLOR_BOLA puede ser → rojo, azul, verde
     public static void main(String[] args) {
-
+        int contador = 0;
+        String color_bola;
+        do{
+            color_bola = getBall();
+            if (color_bola.equals("azul")){
+                contador++;
+            }
+            System.out.println("La bola es de color: " + color_bola);
+        }while (contador < 2);
     }
 
-    //TODO ↓
-    // Un método que devuelva una cadena de texto con el color de una pelota (en minúscula). Dicho color se escogerá de
-    // manera aleatoria. Se empleará el método randomWithRange(int min, int max) para escoger un número aleatorio, que
+    // Un metodo que devuelva una cadena de texto con el color de una pelota (en minúscula). Dicho color se escogerá de
+    // manera aleatoria. Se empleará el metodo randomWithRange(int min, int max) para escoger un número aleatorio, que
     // lo asociaremos al valor de un color de la siguiente manera:
     // 1 → rojo
     // 2 → azul
     // 3 → verde
     public static String getBall() {
-        return null;
+
+        String color;
+
+        switch (randomWithRange(1,4)){
+            case 1:
+                color = "rojo";
+                break;
+            case 2:
+                color = "azul";
+                break;
+            case 3:
+                color = "verde";
+                break;
+            default:
+                color = "";
+        }
+
+        return color;
     }
 
     public static int randomWithRange(int min, int max) {
